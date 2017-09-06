ab4cace451cdf87115d216d89bb5e9f45b4fb649
Dalejannirmalkar <dalejannirmalkar@gmail.com>
2017-09-06 14:07:26 +0530

    Initial commit

---
+++ app/src/main/java/app/abhijeet/mrdhwaj/dialogboxesbar/MainActivity.java
@@ -0,0 +1,25 @@
+package app.abhijeet.mrdhwaj.dialogboxesbar;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Dialogshow(View view){
+        Intent i1=new Intent(MainActivity.this,Main2Activity.class);
+        startActivity(i1);
+    }
+}
